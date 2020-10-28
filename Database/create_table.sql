fc2e776a4228e532cc09ed783c2c3a88c28e61ad
Sean Yao <ckyao2@illinois.edu>
2020-10-28 04:22:33 -0500

    create_table.sql

--- Database/create_table.sql
+++ Database/create_table.sql
@@ -6,7 +6,7 @@ CREATE TABLE Member(
 
 CREATE TABLE Home(
 	homeID 	INT unsigned AUTO_INCREMENT, 
-	memberID 	INT unsigned AUTO_INCREMENT,
+	memberID 	INT unsigned,
 	homeName	CHAR(20) NOT NULL ,
 	PRIMARY KEY(homeID, memberID),
 	FOREIGN KEY (memberID) REFERENCES Member(memberID)
@@ -14,7 +14,7 @@ CREATE TABLE Home(
 
 CREATE TABLE Grocery(
 	groceryID 	INT unsigned AUTO_INCREMENT, 
-	memberID	INT unsigned AUTO_INCREMENT, 
+	memberID	INT unsigned, 
 	unitPrice	REAL unsigned, 
 	quantity	INT unsigned DEFAULT 0, 
 	purchaseDate	DATE NOT NULL, 
@@ -26,7 +26,7 @@ CREATE TABLE Grocery(
 
 CREATE TABLE Chore(
 	choreID	INT unsigned AUTO_INCREMENT, 
-	memberID	INT unsigned AUTO_INCREMENT, 
+	memberID	INT unsigned, 
 	name		VARCHAR(20), 
 	dueDate	DATE NOT NULL, 
 	status		VARCHAR(5), 
@@ -37,7 +37,7 @@ CREATE TABLE Chore(
 
 CREATE TABLE Contain(
 	memberID 	INT unsigned AUTO_INCREMENT,
-	homeID 	INT unsigned AUTO_INCREMENT, 
+	homeID 	INT unsigned, 
 	PRIMARY KEY (memberID,homeID),
 	FOREIGN KEY (memberID) REFERENCES Member(memberID),
 	FOREIGN KEY (homeID) REFERENCES Home(homeID)
@@ -45,7 +45,7 @@ CREATE TABLE Contain(
 
 CREATE TABLE Shop(
 	groceryID	INT unsigned AUTO_INCREMENT,
-	homeID	INT unsigned AUTO_INCREMENT,
+	homeID	INT unsigned,
 	PRIMARY KEY(groceryID, homeID),
 	FOREIGN KEY (groceryID) REFERENCES Grocery(groceryID),
 	FOREIGN KEY (homeID) REFERENCES Home(homeID),
@@ -55,7 +55,7 @@ CREATE TABLE Shop(
 
 CREATE TABLE Does(
 	choreID	INT unsigned AUTO_INCREMENT,
-	homeID	INT unsigned AUTO_INCREMENT,
+	homeID	INT unsigned,
 	PRIMARY KEY(choreID, homeID),
 	FOREIGN KEY (choreID) REFERENCES Chore(choreID),
 	FOREIGN KEY (homeID) REFERENCES Home(homeID),
