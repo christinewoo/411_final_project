0077feb1727206fb3af8dd54c31cd724bde0b1e1
Christine Wu <wuc6@mca.org.tw>
2020-11-05 12:22:55 +0800

    added INSERT DELETE for members

--- Backend/dozenDuty/dozenDuty_app/urls.py
+++ Backend/dozenDuty/dozenDuty_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='dozenDuty_home'),
     path('groceries/', views.groceries, name='dozenDuty_groceries'),
     path('chores/', views.chores, name='dozenDuty_chores'),
     path('members/', views.members, name='dozenDuty_members'),
+    path('addMember/', views.addMember),
+    path('removeMember/<int:id>/', views.removeMember),
 ]
--- Backend/dozenDuty/dozenDuty_app/views.py
+++ Backend/dozenDuty/dozenDuty_app/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import Member
 
 def home(request):
     return render(request, 'dozenDuty_app/home.html')
 
 def groceries(request):
+	#groceries = GroceryItem.objects.all()
     return render(request, 'dozenDuty_app/groceries.html')
+    			  #{'groceries': groceries})
 
 def chores(request):
     return render(request, 'dozenDuty_app/chores.html')
 
 def members(request):
-    return render(request, 'dozenDuty_app/members.html')
+	members = Member.objects.all()
+	return render(request, 'dozenDuty_app/members.html', {'members': members})
+
+def addMember(request):
+	name = request.POST['memberName']
+	new_item = Member(memberName=name)
+	new_item.save()
+	return HttpResponseRedirect('/members/')
+
+def removeMember(request, id):
+	to_remove = Member.objects.get(memberID=id)
+	to_remove.delete()
+	return HttpResponseRedirect('/members/')
