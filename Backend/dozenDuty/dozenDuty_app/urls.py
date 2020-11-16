e753307ac84aaf2f14d7e67ebba0909c8c89f38f
Sean Yao <ckyao2@illinois.edu>
2020-11-16 10:45:38 -0600

    add comments

--- Backend/dozenDuty/dozenDuty_app/urls.py
+++ Backend/dozenDuty/dozenDuty_app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.main, name='dozenDuty_main'),
 
+    """Groceries"""
     path('groceries/', views.groceries, name='groceries'),
     path('groceries/add/', views.addGrocery, name='groceries-add'),
     path('groceries/<int:id>/detail/', views.detailGrocery, name='groceries-detail'),
@@ -12,13 +13,15 @@ urlpatterns = [
     path('groceries/<int:id>/update/', views.updateGrocery, name='groceries-update'),
     path('groceries/search/', views.searchGrocery, name='groceries-search_results'),
 
+    """Chores"""
     path('chores/', views.chores, name='chores'),
     path('chores/add/', views.addChores, name='chores-add'),
     path('chores/<int:id>/detail/', views.detailChores, name='chores-detail'),
     path('chores/<int:id>/remove/', views.removeChores, name='chores-remove'),
     path('chores/<int:id>/update/', views.updateChores, name='chores-update'),
     path('chores/search/', views.searchChores, name='chores-search_results'),
-
+    
+    """Members"""
     path('members/', views.members, name='dozenDuty_members'),
     path('members/add/', views.addMember, name='members-add'),
     path('members/<int:id>/remove/', views.removeMember, name='members-remove'),
